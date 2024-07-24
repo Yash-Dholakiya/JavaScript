029a17524a5608e2c2fca783b810ea3f3864108a
codyyash1012 <132637690+codyyash1012@users.noreply.github.com>
2024-07-24 13:47:34 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 > 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);  //greater than
+console.log(null == 0); //equal to
+console.log(null >= 0); //greater than or equal to
+console.log(null === 0);    //equal value and equal type
+console.log(null != 0); //not equal
+console.log(null !== 0);    //not equal value or not equal type
+console.log(null < 0);  //less than
+console.log(null <= 0); //less than or equal to	
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log("2" === 2);
