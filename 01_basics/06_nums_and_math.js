4d844615f31847dd716b6150852101135ded7a67
Yash Dholakiya <132637690+Yash-Dholakiya@users.noreply.github.com>
2025-03-11 05:43:14 +0000

    nums and maths

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -12,17 +12,17 @@ const otherNumber = 123.8966
 // console.log(otherNumber.toPrecision(4));
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-IN')); //comas
 
 //+++++++++++++++++++++++ Maths +++++++++++++++++++++++
 
 // console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
-// console.log(Math.min(4,3,5,9,8));
-// console.log(Math.max(4,3,5,9,8));
+// console.log(Math.abs(-4)); // negative to positive
+// console.log(Math.round(4.6)); //round of
+// console.log(Math.ceil(4.2)); //top value 
+// console.log(Math.floor(4.9)); //lower value
+// console.log(Math.min(4,3,5,9,8)); //
+// console.log(Math.max(4,3,5,9,8)); //
 
 console.log(Math.random());
 console.log((Math.random()*10) + 1);
